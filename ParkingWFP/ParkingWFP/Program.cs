3c682367dd7497039c830f894dc710a12da9044f
Mariany Ferreira da Silva (myf) <fsilvamariany@gmail.com>
2018-10-19 20:57:51 -0300

    [PK-001] Rename Form1 to Main

--- ParkingWFP/ParkingWFP/Program.cs
+++ ParkingWFP/ParkingWFP/Program.cs
@@ -16,7 +16,7 @@ namespace ParkingWFP
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
